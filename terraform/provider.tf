86c92b0c498f835014bc6ce65e7768ea1ef5170b
Hein Htet Win <48096774+heinhtetwin@users.noreply.github.com>
2023-01-15 01:12:23 +0630

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIA6DWJ4FU2B7YW6VWI"
-  secret_key = "14e19LweUJmsHDeCVgQnvREDxlEg32DdcbG6fL23"
+  access_key = <your-access-key>
+  secret_key = <your-secret-key>
   region = var.region
 }
